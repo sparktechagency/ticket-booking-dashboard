e79d183a61bc995da2ee4c8fe7dbc6f89862eb00
nrshakib <nrshakib007@gmail.com>
2026-01-27 14:20:22 +0600

    Added .env

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     // host: "0.0.0.0",
-    host: "72.62.190.141",    
+    // host: "72.62.190.141",    
     port: 1001,
   },
 });
